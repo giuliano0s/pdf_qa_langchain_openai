1f1df7b5a86569a0ff25532d769eb1804238750d
Giuliano3530 <giuliano.souza@gavb.com.br>
2024-03-19 17:07:17 -0300

    streamlit chat/llm integration

--- README.md
+++ README.md
@@ -2,8 +2,14 @@
 Simple PDF QA using RAG on documents from IFRS Canoas.
 
 # Use
-Put your OpenAI API key on 'OPENAI_KEY' variable, then put unprocessed documents(PDF) on ./data/unprocessed_data and run the code. It will process the document and save it in a vector store.
-Documents only need to be processed once.
+### Default documents
+Go to run_app.ipynb, select your venv's kernel and run the code.
+Put your OpenAI API key in the sidebar variable and then validate your api.
+Once validated, you can chat with the model.
 
-Put your question in the last cell, on 'msg' variable.
-The model is aware of chat history.
+### Your documents
+Put unprocessed documents(PDF) on ./data/unprocessed_data. Documents only need to be processed once.
+Go to run_app.ipynb, select your venv's kernel and run the code.
+Put your OpenAI API key in the sidebar variable and then validate your api.
+Your PDF will be processed for better understanding of the LLM. Documents only need to be processed once. (can take a few minutes depending on the document's size)
+After that, you can talk to the model.
--- app.py
+++ app.py
@@ -1,55 +1,191 @@
 import streamlit as st
 
-# Título da página
-st.title('Chat')
+# Model/parser
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
-user_name = 'Usuário'
+# Embedding/vector store
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-# Inicializando o histórico de chat
-if 'messages' not in st.session_state:
-    st.session_state.messages = []
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain.document_loaders import PyPDFLoader
 
-# Placeholder para mensagens
-chat_history = st.empty()
+import time
 
-# CSS para fixar a caixa de texto na parte inferior e estilo do histórico de mensagen
-st.markdown("""
-    <style>
-        .chat-messages {
-            height: 300px;
-            overflow-y: auto;
-            display: flex;
-            flex-direction: column-reverse;
-            border: 1px solid #ccc;
-            padding: 10px;
-            margin-bottom: 20px;
-        }
-        .fixed-bottom {
-            position: fixed;
-            bottom: 0;
-            left: 0;
-            width: 100%;
-            background-color: white;
-        }
-        /* Corrigir a posição do footer */
-        .streamlit-footer {
-            padding-bottom: 70px;
-        }
-    </style>
-""", unsafe_allow_html=True)
+# System
+import os
 
-with st.form("form_message", clear_on_submit=True):
-    st.markdown('<div class="fixed-bottom">', unsafe_allow_html=True)
-    # Caixa de texto para a mensagem dentro de um formulário
-    user_message = st.text_input(f'{user_name}, digite sua mensagem:', key="message_input")
+#%%#################################################### FUNCTIONS
 
-    # Botão para enviar mensagem
-    send_message = st.form_submit_button('Enviar Mensagem')
+def check_last_api_input():
+    lst = [x for x in st.session_state.log_messages if x in ['API Validated','Please, insert a valid API key']]
+    return lst
 
-    st.markdown('</div>', unsafe_allow_html=True)
+def write_logs(new_message=False):
+    if new_message:
+        st.session_state.log_messages.insert(0, new_message)
 
-if send_message and user_message:
-        st.session_state.messages.append(f"{user_name}: {user_message}")
+    for message in st.session_state.log_messages:
+        st.sidebar.text(message)
 
-# Exibir mensagens (invertido, as novas mensagens aparecem no topo)
-st.markdown('<div class="chat-messages">' + '<br>'.join(reversed(st.session_state.messages)) + '</div>', unsafe_allow_html=True)
+def validate_key():
+    try:
+        ChatOpenAI(openai_api_key=OPENAI_API_KEY).invoke("Testing key")
+        return True
+    except:
+        return False
+
+#%%#################################################### SIDEBAR
+
+# Title
+st.sidebar.title('PDF Q&A')
+
+# Start log messages
+if 'log_messages' not in st.session_state:
+    st.session_state.log_messages = ['Please, insert a valid API key']
+
+# Reload files button
+reload_button = st.sidebar.button('Reload files')
+if reload_button:
+    try:
+        st.session_state.pop('vector')
+        st.session_state.log_messages = ['Please, validate your API key again']
+        st.sidebar.write('Files updated')
+    except:
+        st.sidebar.write('Nothing to update')
+
+# Validade API key button
+OPENAI_API_KEY = st.sidebar.text_input('OPENAI_API_KEY:')
+
+validate_button = st.sidebar.button('Validate key')
+if validate_button:
+    st.session_state.valid_key = validate_key()
+
+    if st.session_state.valid_key:
+        st.session_state.log_messages.insert(0, 'API Validated')
+    else:
+        st.session_state.log_messages.insert(0, 'Please, insert a valid API key')
+
+else:
+    if 'valid_key' not in st.session_state:
+        st.session_state.valid_key = False
+
+    st.sidebar.markdown('### Logs')
+    write_logs()
+
+#%%#################################################### READ FILES
+
+if ('vector' not in st.session_state) and (st.session_state.valid_key):
+
+    # Scan folder
+    files = os.listdir('./data/unprocessed_data/')
+    files = [x for x in files if x[-4:]=='.pdf']
+    embeddings = OpenAIEmbeddings(openai_api_key = OPENAI_API_KEY)
+
+    if len(files)>0:
+        write_logs(f'{len(files)} new files found, processing and merging')
+
+        #Load files
+        loaders = [PyPDFLoader('./data/unprocessed_data/' + x) for x in files]
+
+        docs = []
+        for loader in loaders:
+            docs.extend(loader.load())
+
+        #Process
+        llm = ChatOpenAI(openai_api_key = OPENAI_API_KEY, model_name = 'gpt-3.5-turbo-16k', temperature=0.5)
+        splits_prompt = ChatPromptTemplate.from_messages([
+            ("system", "Formate o texto para que fique entendível para usar de contexto em uma LLM. me dê apenas o output como resposta:"),
+            ("user", "{input}")
+        ])
+        splits_parser = StrOutputParser()
+        splits_chain = splits_prompt | llm | splits_parser
+        for n in range(len(docs)):
+            docs[n].page_content = splits_chain.invoke({"input": f"{docs[n].page_content}"})
+
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=150)
+        splits = text_splitter.split_documents(docs)
+
+        #Vector merging
+        vector = FAISS.from_documents(splits, embeddings)
+        old_vector = FAISS.load_local("./data/faiss_index", embeddings, allow_dangerous_deserialization=True)
+        vector.merge_from(old_vector)
+
+        vector.save_local("./data/faiss_index")
+
+        for file in files:
+            os.replace(f"./data/unprocessed_data/{file}", f"./data/processed_data/{file}")
+
+        write_logs('Finished processing and merging')
+
+    else:
+        vector = FAISS.load_local("./data/faiss_index", embeddings, allow_dangerous_deserialization=True)
+        time.sleep(1)
+        write_logs('0 new files found, loading old vector')
+
+    st.session_state.vector = vector
+
+#%%#################################################### MODEL
+
+if ('vector' in st.session_state) and (st.session_state.valid_key) and ('started' not in st.session_state):
+    #Chain creation
+
+    st.session_state.started = True
+
+    llm = ChatOpenAI(openai_api_key = OPENAI_API_KEY, model_name = 'gpt-3.5-turbo-16k', temperature=0.5)
+
+    prompt = ChatPromptTemplate.from_template(
+        """"Você deve memorizar os editais do IFRS canoas para responder questões sobre ele.
+
+            Cursos de Licenciatura, Bacharel e Tecnólogo são considerados cursos de nível superior.
+
+            Dê informações completas.
+
+            <context>
+            {context}
+            </context>
+
+            Historico de mensagens: {history}
+
+            Pergunta: {input}""")
+
+    #retrieval
+    document_chain = create_stuff_documents_chain(llm, prompt)
+
+    retriever = st.session_state.vector.as_retriever(search_kwargs={'k': 10, 'score_treshold': 0.9},
+                                                        search_type="similarity")
+    st.session_state.retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+#%%#################################################### CHAT
+
+if ('vector' in st.session_state) and (st.session_state.valid_key) and ('started' in st.session_state):
+
+    # Init. chat history
+    if 'messages' not in st.session_state:
+        st.session_state.messages = []
+        st.session_state.chat_hist = []
+        st.markdown('<div class="chat-messages">' + '<br>'.join(reversed(st.session_state.messages)) + '</div>', unsafe_allow_html=True)
+
+    # Messages form
+    with st.form("form_message", clear_on_submit=True):
+
+        st.markdown('<div class="fixed-bottom">', unsafe_allow_html=True)
+        user_message = st.text_input('Type your message:', key="message_input")
+        send_message = st.form_submit_button('Send message')
+        st.markdown('</div>', unsafe_allow_html=True)
+
+    if send_message and user_message:
+            st.session_state.messages.append(f"User: {user_message}")
+            st.session_state.messages.append(f"_______________________________________________________")
+            #st.markdown('<div class="chat-messages">' + '<br>'.join(reversed(st.session_state.messages)) + '</div>', unsafe_allow_html=True)
+
+            response = st.session_state.retrieval_chain.invoke({"input": user_message, "history": st.session_state.chat_hist})
+            st.session_state.messages.append(f"ChatGPT: {response['answer']}")
+            st.session_state.messages.append(f"_______________________________________________________")
+
+    # Show messages
+    st.markdown('<div class="chat-messages">' + '<br><br>'.join(reversed(st.session_state.messages)) + '</div>', unsafe_allow_html=True)
